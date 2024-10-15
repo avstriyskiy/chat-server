f8ab0af877299376ad4816eba572d8f2b8ffca72
Nikita Avstriyskiy <smileneverdie@gmail.com>
2024-10-15 23:38:24 +0300

    feat: added database saving

--- cmd/server/routes.go
+++ cmd/server/routes.go
@@ -4,29 +4,64 @@ import (
 	"context"
 	"log"
 
+	sq "github.com/Masterminds/squirrel"
 	chatService "github.com/avstriyskiy/chat-server/pkg/chat_v1"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 // Create method
-func (s *chatServer) Create(_ context.Context, req *chatService.CreateRequest) (*chatService.CreateResponse, error) {
-	log.Printf("Created chat with users: %v", req.GetUsernames())
+func (s *chatServer) Create(ctx context.Context, req *chatService.CreateRequest) (*chatService.CreateResponse, error) {
+	log.Printf("creating chat with users: %v", req.GetUsernames())
+
+	query, args, err := sq.Insert("chats").
+		PlaceholderFormat(sq.Dollar).
+		Columns("usernames").
+		Values(req.GetUsernames()).
+		Suffix("RETURNING id").ToSql()
+	if err != nil {
+		log.Printf("failed to build query: %v", err)
+		return nil, err
+	}
+	log.Printf("query: %s, args: %v", query, args)
+
+	var chatID int
+	if err = s.pool.QueryRow(ctx, query, args...).Scan(&chatID); err != nil {
+		log.Printf("failed to create chat: %v, query: %s", err, query)
+		return nil, err
+	}
+
+	log.Printf("successfully created chat with users: %v", req.GetUsernames())
 
 	return &chatService.CreateResponse{
-		Id: 888,
+		Id: int64(chatID),
 	}, nil
 }
 
 // SendMessage method
-func (s *chatServer) SendMessage(_ context.Context, req *chatService.SendMessageRequest) (*emptypb.Empty, error) {
+func (s *chatServer) SendMessage(ctx context.Context, req *chatService.SendMessageRequest) (*emptypb.Empty, error) {
 	log.Printf("sending message to chat from %s, message: %s", req.GetFrom(), req.GetText())
 
 	return &emptypb.Empty{}, nil
 }
 
 // Delete method
-func (s *chatServer) Delete(_ context.Context, req *chatService.DeleteRequest) (*emptypb.Empty, error) {
-	log.Printf("Deleting user ID: %d", req.GetId())
+func (s *chatServer) Delete(ctx context.Context, req *chatService.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("deleting chat ID: %d", req.GetId())
+
+	query, args, err := sq.Delete("chats").
+		PlaceholderFormat(sq.Dollar).
+		Where(sq.Eq{"id": req.GetId()}).ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = s.pool.Exec(ctx, query, args...)
+	if err != nil {
+		log.Printf("failed to delete chat: %v", err)
+		return nil, err
+	}
+
+	log.Printf("successfully deleted chat ID: %d", req.GetId())
 
 	return &emptypb.Empty{}, nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/avstriyskiy/chat-server
 go 1.21
 
 require (
+	github.com/Masterminds/squirrel v1.5.4
 	github.com/jackc/pgx/v4 v4.18.3
 	google.golang.org/grpc v1.65.0
 	google.golang.org/protobuf v1.34.2
@@ -17,6 +18,8 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
+	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
